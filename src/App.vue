230b4b4af68b8a8c09373227b091d474eb924534
palashkumardas <info.palash27@gmail.com>
2023-07-10 09:47:53 -0700

    js expression

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,23 @@
 <script setup>
 
-      const image='https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80'
-      const title= 'Image title one'
-      const link= 'https://unsplash.com/photos/NRQV-hBF10M'
+    const number = 17
+
+    function calculateData(n){
+      if(n % 2 ==0){
+        return "even"
+      }
+      else{
+        return"odd"
+      }
+    }
     
 </script>
 
 <template>
-      <section class="container mx-auto p-10">
-        <h1 class="text-4xl text-center mb-10">Data Project</h1>
-        <section class="main flex space-x-4">
-            <div class="flex space-y-4 flex-col w-1/3">
-                <img :title="title" :src="image" :alt="title">
-                <h2 class="text-xl text-left">{{ title }}</h2>
-                <a target="_blank" :href="link" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded w-40 text-center">
-                    Image Source
-                </a>
-            </div>
-        </section>
-    </section>
+      <div>
+        <h1>The number is : {{ number }} / 4 = {{ number/4 }}</h1>
+        <h2>The result is:{{ number %2 ==0 ? "even" :"odd" }}</h2>
+      </div>
 </template>
 
 <style scoped>
